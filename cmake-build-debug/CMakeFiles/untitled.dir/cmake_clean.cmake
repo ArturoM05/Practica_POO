4676b33a10453b7b148cf9904cf44160b618dc1d
Juan Miguel Arriola Mazo <141200068+arriola420@users.noreply.github.com>
2023-09-11 22:15:28 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/Datos.cpp.obj"
+  "CMakeFiles/untitled.dir/Datos.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Nodo.cpp.obj"
+  "CMakeFiles/untitled.dir/Nodo.cpp.obj.d"
+  "CMakeFiles/untitled.dir/list2linked.cpp.obj"
+  "CMakeFiles/untitled.dir/list2linked.cpp.obj.d"
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
